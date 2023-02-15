ad52f7bf37ad3bedf568532691c95efae1cd3502
Vsevolod-PC <VsevolodMalevskiy@gmail.com>
2023-02-15 20:40:00 +0200

    claas_work_lesson_5

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -25,12 +25,12 @@ SECRET_KEY = 'django-insecure-tjiwi$+_qta@5*n0g7-87il==lg+d$%&^o7+&pcj6*08zl+&3!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,17 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+EXTERNAL_APPS = [
+    'django_extensions'
+]
+
+INTERNAL_APPS = [
+    'currency'
+]
+
+INSTALLED_APPS = DJANGO_APPS + EXTERNAL_APPS + INTERNAL_APPS
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from currency.views import list_rates
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rate/list', list_rates)
 ]
