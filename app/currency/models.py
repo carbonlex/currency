cfb71ea6a6d968af5287279b390a705f5ca1d097
Vsevolod-PC <VsevolodMalevskiy@gmail.com>
2023-02-16 17:44:08 +0200

    home_work_lesson_5

--- app/currency/models.py
+++ app/currency/models.py
@@ -7,3 +7,10 @@ class Rate(models.Model):
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sell = models.DecimalField(max_digits=6, decimal_places=2)
     source = models.CharField(max_length=25)
+
+
+class ContactUs(models.Model):
+    email_from = models.EmailField(max_length=60)
+    subject = models.CharField(max_length=40)
+    message = models.TextField()
+
